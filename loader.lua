ad01c9bb20451f86249241f28a543a5d929847d2
sstvskids <staavstudios@gmail.com>
2025-08-21 19:22:08 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -25,6 +25,12 @@ shared.Settings = loadstring(readfile('journey.lol/cfg.lua'))() or {
     }
 }
 
+for _, v in {'journey.lol'} do
+    if not isfolder(v) then
+        makefolder(v)
+    end
+end
+
 local function getURL(url)
 	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..scripturl, true)
 end
