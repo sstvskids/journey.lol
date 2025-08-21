510a40e4758b284b8d4d7d063f59bf80e76ddcd4
sstvskids <staavstudios@gmail.com>
2025-08-21 19:23:04 +1000

    Fix getURL

--- loader.lua
+++ loader.lua
@@ -32,7 +32,7 @@ for _, v in {'journey.lol'} do
 end
 
 local function getURL(url)
-	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..scripturl, true)
+	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..url, true)
 end
 
 return loadstring(getURL('main.lua'))()
--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local playersService = cloneref(game:GetService('Players'))
 local lplr = playersService.LocalPlayer
 
 local function getURL(url)
-	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..scripturl, true)
+	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..url, true)
 end
 
 local Notifications = loadstring(getURL('notif.lua'))()
