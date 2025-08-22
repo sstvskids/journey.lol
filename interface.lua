348af5cbf644ecf94934551079447da587c8d8eb
sstvskids <staavstudios@gmail.com>
2025-08-22 18:23:36 +1000

    Update interface.lua

--- interface.lua
+++ interface.lua
@@ -33,7 +33,7 @@ local Library = {
 
 function Library:disconnect()
 	for _, value in Library.connections do
-		if not Library.connections[value] and Library.connections[i] == nil then
+		if type(value) ~= 'RBXScriptConnection' then
 			continue
 		end
 
