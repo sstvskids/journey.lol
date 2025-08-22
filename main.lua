b65ad7833c590ca4b1fa0a3fb57b832bc948d665
sstvskids <staavstudios@gmail.com>
2025-08-22 18:40:32 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
     journey.lol
     by @stav
 
-    w-w-wait? a gui-less script!?!
+    Atonium, but undetected ahh script
 ]]
 
 local run = function(func)
@@ -264,33 +264,5 @@ run(function()
         end
     })
 end)
-
--- Features
---[[shared.Settings.Aura.Func = function(callback)
-    if callback then
-        shared.connections.Aura = runService.PreSimulation:Connect(function()
-            task.spawn(function()
-                for _, v in playersService:GetPlayers() do
-                    if v ~= lplr and isAlive(v) and (getPart(lplr).Position - getPart(v).Position).Magnitude <= shared.Settings.Aura.Config.Range then
-                        if shared.Settings.Aura.Config.TPPlr == true then
-                            getPart(lplr).CFrame = getPart(v).CFrame + Vector3.new(0, 1, 0)
-                        end
-
-                        workspace.CombatSystemFolder.PannelFolder.Remotes.PunchRemote:FireServer('LeftPunch', {}, 0)
-                        workspace.CombatSystemFolder.PannelFolder.Remotes.PunchRemote:FireServer('RightPunch', {}, 0)
-                    end
-                end
-            end)
-        end)
-    else
-        if shared.connections.Aura then
-            shared.connections.Aura:Disconnect()
-        end
-    end
-end]]
-
-for _, value in interface.connections do
-    print(_, value)
-end
 
 return Notifications.NewNotification(lplr, 'Script fully loaded :)', 4, Color3.fromRGB(255,255,255), 'Yay!')
