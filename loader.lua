35ed245e9c1fea57e15f1a65285c410cae418d7d
sstvskids <staavstudios@gmail.com>
2025-08-22 16:39:21 +1000

    Add interface

--- loader.lua
+++ loader.lua
@@ -1,44 +1,12 @@
 --[[
-    to get the latest updates: always come here
+
+    journey.lol
+    by @stav
+
+    w-w-wait? a gui-less script!?!
 ]]
 
 repeat task.wait() until game:isLoaded()
-
-local cloneref = cloneref or function(obj)
-    return obj
-end
-
-local userInputService = cloneref(game:GetService('UserInputService'))
-local httpService = cloneref(game:GetService('HttpService'))
-local playersService = cloneref(game:GetService('Players'))
-local lplr = playersService.LocalPlayer
-
-if userInputService.TouchEnabled then
-    return warn('Not supported on mobile; computer only')
-end
-
-if not shared.Settings then
-    shared.Settings = isfile('journey.lol/cfg.lua') and loadstring(httpService:JSONDecode(readfile('journey.lol/cfg.lua')))() or {
-        DoubleXP = {
-            Enabled = true,
-            Config = {},
-            Func = ''
-        },
-        Aura = {
-            Enabled = true,
-            Config = {
-                Range = 15,
-                TPPlr = false
-            },
-            Func = ''
-        }
-    }
-end
-
-if shared.uninject then
-    setclipboard('https://github.com/sstvskids/journey.lol')
-    return shared.notifications.NewNotification(lplr, 'Documentation on how to config has been copied to your clipboard', 4, Color3.fromRGB(255,255,255), 'Yay!')
-end
 
 for _, v in {'journey.lol'} do
     if not isfolder(v) then
--- main.lua
+++ main.lua
@@ -6,6 +6,10 @@
     w-w-wait? a gui-less script!?!
 ]]
 
+local run = function(func)
+    local suc, res = pcall(func)
+    return suc == true and res or suc == false and ((writefile and writefile('journey.lol/consoleerr.txt', res)) or warn(res))
+end
 local cloneref = cloneref or function(obj)
     return obj
 end
@@ -20,6 +24,7 @@ local function getURL(url)
 	return game:HttpGet('https://raw.githubusercontent.com/sstvskids/journey.lol/'..httpService:JSONDecode(game:HttpGet('https://api.github.com/repos/sstvskids/journey.lol/commits'))[1].sha..'/'..url, true)
 end
 
+local interface = loadstring(getURL('interface.lua'))()
 local Notifications = loadstring(getURL('notif.lua'))()
 shared.notifications = Notifications
 
@@ -27,13 +32,6 @@ local savetog, lastsave = true, os.clock()
 
 if not shared.connections or not type(shared.connections) == 'table' then
     shared.connections = {}
-end
-
-local function save()
-    if savetog == true and os.clock() - lastsave >= 5 then
-        lastsave = os.clock()
-        writefile('journey.lol/config.lua', httpService:JSONEncode(shared.Settings))
-    end
 end
 
 local function isAlive(v)
@@ -46,36 +44,84 @@ local function getPart(v)
     end
 end
 
+local main = interface.new()
+local tabs = {
+    Combat = main.create_tab('Main'),
+    Blatant = main.create_tab('Blatant'),
+    Render = main.create_tab('Render'),
+    Settings = main.create_tab('Settings')
+}
 
--- Core Functions
-shared.disconnect = function()
-    for i,v in shared.connections do
-        if v and type(v) == 'RBXScriptConnection' then
-            v:Disconnect()
-            v = nil
+run(function()
+    tabs.Combat.create_title({
+        name = 'Aura',
+        section = 'left'
+    })
+
+    local TPAura
+    local Range
+    tabs.Combat.create_toggle({
+        name = 'Aura',
+        section = 'left',
+        callback = function(callback)
+            if callback then
+                interface.connections.Aura = runService.PreSimulation:Connect(function()
+                    task.spawn(function()
+                        for _, v in playersService:GetPlayers() do
+                            if v ~= lplr and isAlive(v) and (getPart(lplr).Position - getPart(v).Position).Magnitude <= 15 then
+                                if TPAura then
+                                    getPart(lplr).CFrame = getPart(v).CFrame + Vector3.new(0, 1, 0)
+                                end
+
+                                workspace.CombatSystemFolder.PannelFolder.Remotes.PunchRemote:FireServer('LeftPunch', {}, 0)
+                                workspace.CombatSystemFolder.PannelFolder.Remotes.PunchRemote:FireServer('RightPunch', {}, 0)
+                            end
+                        end
+                    end)
+                end)
+            else
+                if shared.connections.Aura then
+                    shared.connections.Aura:Disconnect()
+                end
+            end
         end
-    end
-end
+    })
+    tabs.Combat.create_slider({
+        name = 'Range',
+        section = 'left',
+        callback = function(callback)
+            Range = callback
+        end
+    })
+    tabs.Combat.create_toggle({
+        name = 'TPAura',
+        section = 'left',
+        callback = function(callback)
+            TPAura = callback
+        end
+    })
+end)
 
-shared.uninject = function()
-    task.spawn(function()
-        save()
-        savetog = false
-    end)
+run(function()
+    tabs.Settings.create_title({
+        name = 'Uninject',
+        section = 'left'
+    })
 
-    for i,v in shared.Settings do
-        v.Enabled = false
-    end
-
-    shared.Settings = nil
-    shared.disconnect()
-
-    shared.disconnect = nil
-    Notifications.NewNotification(lplr, 'Uninjected :(', 4, Color3.fromRGB(255,255,255), 'Yay!')
-end
+    tabs.Settings.create_toggle({
+        name = 'Uninject',
+        section = 'left',
+        callback = function(callback)
+            if callback then
+                Notifications.NewNotification(lplr, 'uninjected', 2, Color3.fromRGB(255,255,255), 'Yay!')
+                interface:Uninject()
+            end
+        end
+    })
+end)
 
 -- Features
-shared.Settings.Aura.Func = function(callback)
+--[[shared.Settings.Aura.Func = function(callback)
     if callback then
         shared.connections.Aura = runService.PreSimulation:Connect(function()
             task.spawn(function()
@@ -96,14 +142,6 @@ shared.Settings.Aura.Func = function(callback)
             shared.connections.Aura:Disconnect()
         end
     end
-end
+end]]
 
--- Init :)
-for i,v in shared.Settings do
-    if type(v.Func) == 'string' then continue end
-    
-    v.Func(v.Enabled)
-end
-
-shared.connections.cfgs = runService.PreSimulation:Connect(save)
 return Notifications.NewNotification(lplr, 'Script fully loaded :)', 4, Color3.fromRGB(255,255,255), 'Yay!')
